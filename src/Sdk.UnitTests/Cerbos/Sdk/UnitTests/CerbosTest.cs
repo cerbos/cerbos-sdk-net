fc346ee3b3a23184e1ad2168995f74901899ee92
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2026-06-04 10:06:11 +0300

    Add README.md section
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- src/Sdk.UnitTests/Cerbos/Sdk/UnitTests/CerbosTest.cs
+++ src/Sdk.UnitTests/Cerbos/Sdk/UnitTests/CerbosTest.cs
@@ -1,7 +1,6 @@
 // Copyright 2021-2026 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Threading.Tasks;
 using Cerbos.Sdk.Builder;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
